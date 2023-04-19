d21a1212ed2bcb2f38b2b43e11d608f179f61fe4
KyleGong-alt <kylegong@ymail.com>
2023-04-19 04:30:46 -0700

    Opera works now

--- scripts/content.js
+++ scripts/content.js
@@ -40,9 +40,8 @@ chrome.runtime.sendMessage({
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.message === 'update_timer_content') {
-    // displayTimer(request.start_timer);
+    console.log('timer updated');
 
-    console.log('request.time');
     displayTimer(request.time);
   } else if (request.message === 'reset_timer_content') {
     clearInterval(intervalID);
@@ -60,24 +59,6 @@ chrome.tabs.onActivated.addListener(function (activeInfo) {
   let difference = Date.now() - startTime;
   console.log('time elapsed', difference);
 });
-
-// document.getElementById('startTimer').addEventListener('click', () => {
-//   chrome.runtime.sendMessage({
-//     message: 'start_timer',
-//   });
-// });
-
-// document.getElementById('pauseTimer').addEventListener('click', () => {
-//   chrome.runtime.sendMessage({
-//     message: 'pause_timer',
-//   });
-// });
-
-// document.getElementById('resetTimer').addEventListener('click', () => {
-//   chrome.runtime.sendMessage({
-//     message: 'reset_timer',
-//   });
-// });
 
 function displayTimer(milliseconds) {
   let copy_of_mil = milliseconds;
@@ -108,6 +89,24 @@ function displayTimer(milliseconds) {
   }, 10);
 }
 
+// document.getElementById('startTimer').addEventListener('click', () => {
+//   chrome.runtime.sendMessage({
+//     message: 'start_timer',
+//   });
+// });
+
+// document.getElementById('pauseTimer').addEventListener('click', () => {
+//   chrome.runtime.sendMessage({
+//     message: 'pause_timer',
+//   });
+// });
+
+// document.getElementById('resetTimer').addEventListener('click', () => {
+//   chrome.runtime.sendMessage({
+//     message: 'reset_timer',
+//   });
+// });
+
 // function iterateTimer(millisecond) {
 //   // let [hours, minutes, seconds] = [0, 0, 0];
 //   // let timerRef = document.querySelector('.timerDisplay');
@@ -132,4 +131,59 @@ function displayTimer(milliseconds) {
 //     //  let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds < 100 ? "0" + milliseconds : milliseconds;
 //     timerRef.textContent = `${h}h : ${m}m : ${s}s`;
 //   }, 10);
+// }
+// }
+// else if (browser === 'opera') {
+//   chrome.runtime.sendMessage({
+//     message: 'update_timer',
+//   });
+
+//   chrome.runtime.onMessage.addListener(function (
+//     request,
+//     sender,
+//     sendResponse
+//   ) {
+//     if (request.message === 'update_timer_content') {
+//       console.log('request.time');
+//       displayTimer(request.time);
+//     } else if (request.message === 'reset_timer_content') {
+//       clearInterval(intervalID);
+//       displayTimer(0);
+//     }
+//   });
+
+//   chrome.tabs.onActivated.addListener(function (activeInfo) {
+//     let startTime = sessionStorage.getItem(activeInfo.tabId);
+//     let difference = Date.now() - startTime;
+//     console.log('time elapsed', difference);
+//   });
+
+//   function displayTimer(milliseconds) {
+//     let copy_of_mil = milliseconds;
+
+//     intervalID = setInterval(() => {
+//       seconds = copy_of_mil / 1000;
+//       if (seconds > 60) {
+//         minutes = seconds / 60;
+//         seconds = seconds - 60 * Math.floor(minutes);
+//         if (minutes > 60) {
+//           hours = minutes / 60;
+//           minutes = minutes - 60 * Math.floor(hours);
+//         }
+//       }
+//       // console.log(hours, minutes);
+
+//       (hours = Math.floor(hours)),
+//         (minutes = Math.floor(minutes)),
+//         (seconds = Math.floor(seconds));
+
+//       let h = hours < 10 ? '0' + hours : hours;
+//       let m = minutes < 10 ? '0' + minutes : minutes;
+//       let s = seconds < 10 ? '0' + seconds : seconds;
+
+//       console.log(h, m, s);
+//       timerRef.textContent = `${h}h : ${m}m : ${s}s`;
+//       copy_of_mil += 10;
+//     }, 10);
+//   }
 // }
